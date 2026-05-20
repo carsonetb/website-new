ddab696d26ed5284c39dafd998cac2949b6efa12
carsonetb <blackhole243243@gmail.com>
2026-05-19 21:26:45 -0700

    Icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	export const prerender = true;
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head
+	><link
+		rel="icon"
+		href="https://github.com/carsonetb/website-main/blob/main/assets/profile.png?raw=true"
+	/></svelte:head
+>
 {@render children()}
